ccb940190e31126e9c1fbcf94dd265ef4635c120
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-04-01 20:24:29 +0800

    Refactor fetch_data function to accept task name and implement concurrent execution in main coroutine

--- corountine3.py
+++ corountine3.py
@@ -1,10 +1,8 @@
 import asyncio
-import threading
-import time
 
-async def fetch_data(inputs, id):
+async def fetch_data(task, inputs, id):
     for i in range(inputs):
-        print(f"Processing input {i + 1}")
+        print(f"Processing task {task} input {i + 1}")
         await asyncio.sleep(1)
         inputs -= 1
         if inputs == 0:
@@ -14,9 +12,17 @@ async def fetch_data(inputs, id):
     
 async def main():
     inputs = int(input("Input the amount of inputs "))
-    id = int(input("input the previous ID "))
-    result = await fetch_data(inputs, id)
-    print(f"Your Id number is {result}")
+    id = int(input("input your ID "))
+    # This comment is sequential not concurrent (Async)
+    # taskA frist then taskB
+    # taskA = fetch_data("Task A",inputs, id)
+    # taskB = fetch_data("Task B", inputs, 100 )
+    t1 = fetch_data("Task A",inputs, id)
+    t2 = fetch_data("Task B", inputs, 100 )
+    taskA = await t1
+    taskB = await t2
+    
+    print(f"Your Id number is {taskA} compared to the id number of task b {taskB}")
     
 asyncio.run(main())
 ####------------one below is basically the same but without asyncio-------------
